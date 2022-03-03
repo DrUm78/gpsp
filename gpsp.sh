ee9db50308de02e028151a60e66d31fad5a62924
root <cruau.david@gmail.com>
2022-03-03 12:56:13 +0100

    Updated shell script and replaced instant_play file to fix the saturated sound issue when using Instant Play

--- gpsp.sh
+++ gpsp.sh
@@ -3,6 +3,8 @@
 if [ `md5sum /usr/games/gpsp | cut -d' ' -f1` != `md5sum gpsp | cut -d' ' -f1` ]; then
 	rw
 	cp -f gpsp /usr/games
+	cp -f gba_launch_gpsp.sh /usr/games/launchers
+	cp -f instant_play /usr/local/sbin
 	ro
 fi
 exec /usr/games/launchers/gba_launch_gpsp.sh "$1"
